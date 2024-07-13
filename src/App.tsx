843a75fbe5e428108ef685de73e9af42ef3d0fbb
Akasht3010 <akashthakkar931@gmail.com>
2024-07-13 17:26:27 +0530

    tutor login

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 
 import UserRegister from './pages/UserRegister'
+import TutorRegister from './pages/tutorRegister'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/user/register" element={<UserRegister />} />
+        <Route path="/tutor/register" element={<TutorRegister />} />
       </Routes>
     </>
   )
---
+++ src/pages/tutorRegister.tsx
@@ -0,0 +1,66 @@
+const tutorRegister = () => {
+  return (
+    <main>
+        <div>
+            <img src="" alt="" />
+        </div>
+
+        <div>
+            <h1>Register as a Tutor</h1>
+            <form>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text"  id="firstName" name="firstName" placeholder="First Name"/>
+                </div>
+
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default tutorRegister
